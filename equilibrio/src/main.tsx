14668439c7b010e363d1ac7685117cf4cf47feca
ryanvetoriano <vetorianosilva@gmail.com>
2025-11-12 23:56:36 -0300

    feat: Adicionando router no main, criando globals.css, inicio da rota home e error e adicionando Outlet no  App.

--- equilibrio/src/main.tsx
+++ equilibrio/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
 import './globals.css'
+import Error from './routes/Error/Error.tsx';
+import Login from './routes/Login/Login.tsx';
+import Home from './routes/Home/Home.tsx';
+import Register from './routes/Register/Register.tsx';
+
+const router = createBrowserRouter([
+  {path:"/", element: <App/>, errorElement:<Error/>, children:[
+    {path:"/", element: <Login/>},
+    {path:"/register", element: <Register/>},
+    {path:"/home", element: <Home/>}
+  ]}
+]); 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
