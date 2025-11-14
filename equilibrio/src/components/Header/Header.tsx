ae1017bfe5955fb9ea1ba4780e5c35186911a396
ryanvetoriano <vetorianosilva@gmail.com>
2025-11-13 22:26:37 -0300

    feat: Criação simples do Header

---
+++ equilibrio/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-white shadow-md px-8 py-4 flex items-center justify-between">
+      <h2 className="text-2xl font-semibold text-[#02353C]">Área do Usuário</h2>
+    </header>
+  );
+}
