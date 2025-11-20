80fb8b5de36da830298b248b18f2124fbb20c0a2
ryanvetoriano <vetorianosilva@gmail.com>
2025-11-20 02:25:35 -0300

    feat: Adicionando ThemeProvider no main

--- equilibrio/src/main.tsx
+++ equilibrio/src/main.tsx
@@ -9,13 +9,15 @@ import Error from "./routes/Error/Error.tsx";
 import Login from "./routes/Login/Login.tsx";
 import Register from "./routes/Register/Register.tsx";
 import Home from "./routes/Home/Home.tsx";
-import MainLayout from "./layouts/MainLayout.tsx"; 
+import MainLayout from "./layouts/MainLayout.tsx";
 import Tarefas from "./routes/Tarefas/Tarefas.tsx";
 import Perfil from "./routes/Perfil/index.tsx";
 import Categorias from "./routes/Categorias/index.tsx";
 import Integrantes from "./routes/Integrantes/index.tsx";
 import FAQ from "./routes/Faq/index.tsx";
 import About from "./routes/About/index.tsx";
+import ThemeProvider from "./context/ThemeProvider.tsx";
+
 
 const router = createBrowserRouter([
   {
@@ -32,9 +34,9 @@ const router = createBrowserRouter([
           { path: "/tarefas", element: <Tarefas /> },
           { path: "/categorias", element: <Categorias /> },
           { path: "/perfil", element: <Perfil /> },
-          { path: "/integrantes", element: <Integrantes/>},
-          { path: "/faq", element: <FAQ/>},
-          { path: "/about", element: <About/>}
+          { path: "/integrantes", element: <Integrantes /> },
+          { path: "/faq", element: <FAQ /> },
+          { path: "/about", element: <About /> }
         ],
       },
     ],
@@ -43,6 +45,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
